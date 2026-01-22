ec2f7e7edf9cb2ab80333e5c5f8c42173f67029f
Rémi Campistron <remi.campistron@gmail.com>
2026-01-22 10:19:34 +0100

    Ajout de l'url et de l'id dans la classe Part

--- src/domain/Part.js
+++ src/domain/Part.js
@@ -4,13 +4,16 @@ export class Part {
     shortDescription;
     longDescription;
     priceCents;
+    imageUrl;
     details;
     categoryId;
-    constructor(name, shortDescription, longDescription, priceCents, details, categoryId) {
+    constructor(id, name, shortDescription, longDescription, priceCents,imageUrl, details, categoryId) {
+        this.id = id;
         this.name = name;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
         this.priceCents = priceCents;
+        this.imageUrl = imageUrl;
         this.details = details;
         this.categoryId = categoryId;
     }
