47f074bdc5f236bba984b6bbb32d3f27ca958f5d
Rémi Campistron <remi.campistron@gmail.com>
2026-01-22 10:43:10 +0100

    ajout de commentaires et changement de repertoire

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 // Import our custom CSS
-import '../scss/styles.scss'
-import {Cart} from '../domain/cart';
-import {CartItem} from '../domain/cartitem';
+import '/scss/styles.scss'
+import {Cart} from '/domain/cart';
+import {CartItem} from '/domain/cartitem';
 
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
-import {PartDAO} from "../dao/PartDao";
+import {PartDAO} from "/dao/PartDao";
+import {CategoryDAO} from "./dao/CategoryDao";
 
-
+// Display the cart in the console
 let cart = new Cart();
 let item = new CartItem(2, 100, 1, 1);
 let item2 = new CartItem(2, 100, 1, 1);
@@ -15,9 +16,13 @@ let item3 = new CartItem(2, 100, 1, 1);
 cart.addItem(item);
 cart.addItem(item2);
 cart.addItem(item3);
+
+// Get the total price of the cart
 let total = cart.totalPriceCents();
 console.log(total);
 
+// Display all parts and categories
 let daoPart = new PartDAO()
 daoPart.findAll().then(parts => console.log(parts))
-
+let daoCategory = new CategoryDAO()
+daoCategory.findAll().then(categories => console.log(categories))
